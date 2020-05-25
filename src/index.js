931f969d2daeef690bc82fe0f11f148ab027f111
Amindri Udugla <amindriudugala@gmail.com>
2020-05-25 23:29:38 +0930

    rendering the nav bat and carousel

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from "./components/NavBar/NavBar";
+import Carousle from "./components/Carousle/Carousle";
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
+ReactDOM.render(<NavBar/>, document.getElementById('header'));
+ReactDOM.render(<Carousle/>, document.getElementById('carousle'));
 ReactDOM.render(
   <React.StrictMode>
     <App />
