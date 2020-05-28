186576564e592d29c00e9e59b90949f17eef674e
Amindri Udugla <amindriudugala@gmail.com>
2020-05-28 21:17:31 +0930

    removing fixed sizes

--- src/components/Carousle/Carousle.js
+++ src/components/Carousle/Carousle.js
@@ -4,8 +4,8 @@ import one from '../../img/1.jpg';
 import two from '../../img/2.jpg';
 import './Carousle.css';
 
-function Carousle () {
-    return(
+function Carousle() {
+    return (
         <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
             <ol className="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"/>
@@ -16,54 +16,71 @@ function Carousle () {
                 <div className="carousel-item active">
                     <img className="d-block w-100" src={zero} alt="First slide"/>
                     <div className="header-text">
-                        <div className="text-center">
-                            <h2>
-                                <span>alwis auto repairs</span>
-                            </h2>
+                        <div className="row justify-content-center">
+                            <h1>
+                                alwis auto repairs
+                            </h1>
+                        </div>
+                        <div className="row justify-content-center">
                             <h5>FOR VEHICLES OF ALL MAKES AND MODELS</h5>
-                            <br/>
-                            <a target="_blank" className="btn-lg btn-theme" href="https://alwis.simplybook.me/v2/">Book Now</a>
-
+                        </div>
+                        <br/>
+                        <div className="row justify-content-center">
+                            <a target="_blank" className="btn-lg btn-theme"
+                               href="https://alwis.simplybook.me/v2/">Book Now</a>
                         </div>
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img className="d-block w-100" src={one} alt="Second slide"/>
                     <div className="header-text">
-                        <div className="text-center">
-                            <h5>SPECIALISED FOR</h5>
-                            <h2>
-                                <span>European Cars</span>
-                            </h2>
-                            <br/>
-                            <a target="_blank" className="btn-lg btn-theme" href="https://alwis.simplybook.me/v2/">Book Now</a>
+                        <div className="row justify-content-center"><h5>SPECIALISED FOR</h5></div>
+                        <div className="row justify-content-center">
+                            <h1>
+                                European Cars
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="row justify-content-center">
+                            <a target="_blank" className="btn-lg btn-theme"
+                               href="https://alwis.simplybook.me/v2/">Book Now</a>
                         </div>
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img className="d-block w-100" src={two} alt="Third slide"/>
                     <div className="header-text">
-                        <div className="text-center">
-                            <h4>FREE QUOTES</h4>
-                            <h2>
-                                <span>FRIENDLY SERVICE</span>
-                            </h2>
-                            <h3>CALL 082612856</h3>
-                            <br/>
-                            <a target="_blank" className="btn-lg btn-theme" href="https://alwis.simplybook.me/v2/">Book Now</a>
+                        <div className="row justify-content-center">
+                            <h5>FREE QUOTES</h5>
                         </div>
+                        <div className="row justify-content-center">
+                            <h1>
+                                FRIENDLY SERVICE
+                            </h1>
+                        </div>
+                        <div className="row justify-content-center">
+                            <h5>CALL 082612856</h5>
+                        </div>
+                        <br/>
+                        <div className="row justify-content-center">
+                            <a target="_blank" className="btn-lg btn-theme"
+                               href="https://alwis.simplybook.me/v2/">Book Now</a>
+                        </div>
+
                     </div>
                 </div>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+               data-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="sr-only">Previous</span>
             </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button"
+               data-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
             </a>
         </div>);
-}
+    }
 
-export default Carousle;
+                export default Carousle;
