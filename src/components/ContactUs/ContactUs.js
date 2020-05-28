346d0e2e36efcc617810874b8d264dd992a0974b
Amindri Udugla <amindriudugala@gmail.com>
2020-05-28 10:49:41 +0930

    complete contact us page

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,16 +1,40 @@
 import React from "react";
-
+import map from "../../img/map.jpg"
+import "./ContactUs.css"
 function ContactUs()
 {
     return(
-        <div className="container">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-6">
+                    <div className="contact-details jumbotron justify-content-md-center">
+                    <div><h3>Address:</h3></div>
+                    <div>
+                        <p>31 Petrova Avenue</p>
+                        <p>Windsor Gardens</p>
+                        <p>SA 5086</p>
+                    </div>
 
-            <p>31 Petrova Avenue</p>
-            <p>Windsor Gardens</p>
-            <p>SA 5086</p>
-            <p>Ph:82612856</p>
-            <p>Mobile:0405782727</p>
-            <p>Email:info@alwisautorepairs.com.au</p>
+                    <br/>
+
+                    <div><h3>Phone:</h3></div>
+                    <div><p>82612856</p></div>
+
+                    <br/>
+
+                    <div><h3>Mobile:</h3></div>
+                    <div><p>0405782727</p></div>
+
+                    <br/>
+
+                    <div><h3>Email:</h3></div>
+                    <div><p>info@alwisautorepairs.com.au</p></div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <img src={map} className="map" alt="location"/>
+                </div>
+            </div>
         </div>
     );
 }
